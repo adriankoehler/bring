e68b7c324c9161efa519a0560da5539c7fb9ed01
Adrian Köhler <adrian.koehler@mailbox.tu-dresden.de>
2017-12-01 20:43:45 +0100

    added memo-app for EMI

---
+++ app/src/main/java/c/example/bring/MainActivity.java
@@ -0,0 +1,184 @@
+/*package c.example.bring;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}*/
+
+package c.example.bring;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.javahelps.memo.db.DatabaseAccess;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private FloatingActionButton btnAdd;
+    //private Button btnAdd;
+    private DatabaseAccess databaseAccess;
+    private List<Memo> memos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.databaseAccess = DatabaseAccess.getInstance(this);
+
+        this.listView = (ListView) findViewById(R.id.listView);
+        //this.btnAdd = (Button) findViewById(R.id.btnAdd);
+        this.btnAdd = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+
+        this.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddClicked();
+            }
+        });
+
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Memo memo = memos.get(position);
+                TextView txtMemo = (TextView) view.findViewById(R.id.txtMemo);
+                if (memo.isFullDisplayed()) {
+                    txtMemo.setText(memo.getShortText());
+                    memo.setFullDisplayed(false);
+                } else {
+                    txtMemo.setText(memo.getText());
+                    memo.setFullDisplayed(true);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        databaseAccess.open();
+        this.memos = databaseAccess.getAllMemos();
+        databaseAccess.close();
+        MemoAdapter adapter = new MemoAdapter(this, memos);
+        this.listView.setAdapter(adapter);
+    }
+
+    public void onAddClicked() {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
+    }
+
+    public void onDeleteClicked(Memo memo) {
+        databaseAccess.open();
+        databaseAccess.delete(memo);
+        databaseAccess.close();
+
+        ArrayAdapter<Memo> adapter = (ArrayAdapter<Memo>) listView.getAdapter();
+        adapter.remove(memo);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onEditClicked(Memo memo) {
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra("MEMO", memo);
+        startActivity(intent);
+    }
+
+    private class MemoAdapter extends ArrayAdapter<Memo> {
+
+
+        public MemoAdapter(Context context, List<Memo> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.layout_list_item, parent, false);
+            }
+
+            ImageView btnEdit = (ImageView) convertView.findViewById(R.id.btnEdit);
+            ImageView btnDelete = (ImageView) convertView.findViewById(R.id.btnDelete);
+            TextView txtDate = (TextView) convertView.findViewById(R.id.txtDate);
+            TextView txtMemo = (TextView) convertView.findViewById(R.id.txtMemo);
+
+            final Memo memo = memos.get(position);
+            memo.setFullDisplayed(false);
+            txtDate.setText(memo.getDate());
+            txtMemo.setText(memo.getShortText());
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onEditClicked(memo);
+                }
+            });
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onDeleteClicked(memo);
+                }
+            });
+            return convertView;
+        }
+    }
+}
